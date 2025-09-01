89d1a2e2055b51be4812c3213f40947d2835dc49
Madan Shahi <fullstackshahi@gmail.com>
2025-09-01 15:40:30 +0530

    Starting Header

--- UI/src/App.jsx
+++ UI/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Registration from "./components/Registration";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
+      <Header />
       <Registration />
     </div>
   );
---
+++ UI/src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="">
+      <div>First Header</div>
+      <div>Second Header</div>
+    </div>
+  );
+};
+
+export default Header;
